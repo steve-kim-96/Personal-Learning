934f877d8f7c0c8ef6ae246967476166df60a860
steve-kim-96 <soungku@hotmail.com>
2021-03-12 17:54:29 +1300

    updating latest changes from desktop

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,22 +1,48 @@
-// Declaring shape of object that is assigned "User"
+// // Declaring shape of object that is assigned "User"
 interface User {
   name: string,
   id: number
 }
-// Correct form of declaring object with interface defined above
+// // Correct form of declaring object with interface defined above
 const user: User = {
   name: 'Steve',
   id: 1
 }
-// incorrect form of declaring object with interface defined above
-const incorrectUser: User = {
-  userName: 'Steve',
-  id: 1
-}
+// // incorrect form of declaring object with interface defined above
+// const incorrectUser: User = {
+//   userName: 'Steve',
+//   id: 1
+// }
 
 function getUser(user): User {
   console.log(user)
   return null
 }
 
-getUser({ name: "Steve", id: 1})
+getUser(user)
+
+// Let's see if optional property is printed
+
+
+// function printName(obj: { first: string; last?: string }) {
+//   // Error - might crash if 'obj.last' wasn't provided!
+//   console.log(obj.last.toUpperCase());
+
+//   // A safe alternative using modern JavaScript syntax:
+//   console.log(obj.last?.toUpperCase());
+// }
+
+// printName({ first: "Bob" });
+// printName({ first: "Alice", last: "Alisson" });
+
+interface Anything {
+  first: string,
+  last?: string
+}
+
+const optional: Anything = {
+  first: 'hello world'
+}
+
+console.log(optional.first)
+console.log(optional.last)
