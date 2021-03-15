847eb5b946f4c86a5d09957111168d0669962a27
steve-kim-96 <soungku@hotmail.com>
2021-03-16 08:33:19 +1300

    add console log to see result

--- TODO/src/components/TodoListItem.tsx
+++ TODO/src/components/TodoListItem.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 export default function TodoListItem({ id, name, complete, toggleTodo }: todoListItem) {
   function handleTodoClick () {
     if (!id) return
+    console.log(id)
     toggleTodo(id)
   }
   return (
