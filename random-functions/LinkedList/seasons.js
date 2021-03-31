6027553c8cb23195896147f01cc32ab16ef01195
steve-kim <soungku@hotmail.com>
2021-03-31 13:25:24 +1300

    Make example of linked list

---
+++ random-functions/LinkedList/seasons.js
@@ -0,0 +1,10 @@
+const LinkedList = require('./linkedList')
+
+const seasons = new LinkedList()
+
+seasons.addToHead('summer')
+seasons.addToHead('winter')
+
+seasons.addToTail('spring')
+
+seasons.printList()
