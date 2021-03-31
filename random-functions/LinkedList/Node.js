bb89c0a8e0ddbe41dc73c912a3c1ec97c3f1a59a
steve-kim <soungku@hotmail.com>
2021-03-31 13:26:14 +1300

    return after if statement run so no error shown

--- random-functions/LinkedList/Node.js
+++ random-functions/LinkedList/Node.js
@@ -4,7 +4,14 @@ class Node {
   this.next = null
   }
   setNextNode (node) {
-    if (node instanceof Node || node === null) this.next = node
+    // I'm trying not to use else statements.
+    // in this case, after I set the next node, I break out of the function so that it doesn't
+    // touch the error portion of function
+    if (node instanceof Node || node === null) { 
+    this.next = node
+    return
+    }
+    // if the IF statement turns false, the function will skip to the error statement
     throw new Error('Next node must be a member of the Node class.')
   }
   getNextNode () {
