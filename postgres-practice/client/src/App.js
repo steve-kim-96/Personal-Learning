f2d4f55926d32171a3618e390f051ffab28db0d4
steve-kim-96 <soungku@hotmail.com>
2021-04-10 13:45:39 +1200

    Create and update frontend for postgres practice

---
+++ postgres-practice/client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import InputTodo from './components/InputTodo'
+
+const App = () => ( 
+  <>
+    <div className='container'>
+    <InputTodo />
+    </div>
+  </>
+)
+
+export default App
---
+++ postgres-practice/client/src/components/InputTodo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InputTodo = () => {
+  return (
+    <>
+      <h1 className='text-center mt-5'>Pern TODO list</h1>
+    </>
+  )
+}
+
+export default InputTodo
