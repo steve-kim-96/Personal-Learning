b3532bc867f5f2f9b7a51c1d1868c5f4712ec65b
steve-kim-96 <soungku@hotmail.com>
2021-04-12 14:30:53 +1200

    update hashmap data structure

--- random-functions/HashMap/hashmap.js
+++ random-functions/HashMap/hashmap.js
@@ -1,11 +1,64 @@
 const LinkedList = require("../LinkedList/linkedList");
-
+const Node = require('../LinkedList/Node')
 class HashMap {
   constructor(size = 0) {
     this.hashmap = new Array(size)
       .fill(null)
       .map(() => new LinkedList())
   }
+
+  //.hash function
+  hash(key) {
+    // need to add each acc of hashcode starting with 0
+    let hashcode = 0
+    for (let i = 0; i < key.length; i++) {
+      hashcode += hashcode + key.charCodeAt(i)
+    }
+    return hashcode % this.hashmap.length
+  }
+
+  //.assign function
+  assign(key, value) {
+    let arrayIndex = this.hash(key)
+    let linkedList = this.hashmap[arrayIndex]
+    // first we need to check if any data exists at all. If no head exists that means there are no nodes in the current list
+    if (!linkedList.head) {
+      linkedList.addToHead({ key, value })
+      return
+    }
+    // next we need to check if within the list there is data associated with the same key
+    // in order to do that we need to iterate through the list as long as the current node exists
+    let current = linkedList.head
+    while(current) {
+      if (current.data.key === key) {
+        current.data = { key, value }
+        return
+      }
+      if (!current.next) {
+        current.next = new Node({ key, value })
+        break
+      }
+      current = current.next
+    }
+  }
+
+  //.retrieve function
+  retrieve(key) {
+    const arrayIndex = this.hash(key)
+    const current = this.hashmap[arrayIndex].head
+    // I need to iterate through the list to match the value in the list with the current
+    while(current) {
+      if (current.data.key === key) {
+        console.log(`\nRetrieving ${current.data.value} from index ${arrayIndex}`)
+        return current.data.value
+      }
+      current = current.next
+    }
+    // if nothing matches then just return null
+    return null
+  }
+
+  
 }
 
 module.exports = HashMap
