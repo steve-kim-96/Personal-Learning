79a3b79ddcf7b962ca2ef12875b7a4d2eff835bf
steve-kim-96 <soungku@hotmail.com>
2021-04-13 17:11:32 +1200

    add notes for recursion

--- random-functions/Recursion/recursion.js
+++ random-functions/Recursion/recursion.js
@@ -8,3 +8,9 @@ function recursiveFactorial(n) {
 const recursiveSolution = recursiveFactorial(5)
 
 console.log(recursiveSolution)
+
+/*
+  You always need: 
+    1. A recursive case --> The condition in which the function will perform an action and call itself
+    2. A base case --> The condition in which the function returns a value instead of calling itself i.e. a stopping condition 
+*/
