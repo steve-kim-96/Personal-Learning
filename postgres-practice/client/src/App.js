1e94cb7c49de9b83b9373b686a4c56f27db09707
steve-kim-96 <soungku@hotmail.com>
2021-04-13 17:10:49 +1200

    update completion of postgres todo app

--- postgres-practice/client/src/App.js
+++ postgres-practice/client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import InputTodo from './components/InputTodo'
+import ListTodos from './components/ListTodos'
 
 const App = () => ( 
   <>
     <div className='container'>
     <InputTodo />
+    <ListTodos/>
     </div>
   </>
 )
--- postgres-practice/client/src/components/InputTodo.js
+++ postgres-practice/client/src/components/InputTodo.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const InputTodo = () => {
+  const [description, setDescription] = useState('')
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault()
+    try {
+      const body = { description }
+      const response = await fetch('http://localhost:5000/todos', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body)
+      })
+      window.location = '/'
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
   return (
     <>
       <h1 className='text-center mt-5'>Pern TODO list</h1>
+      <form onSubmit={onSubmitForm} className='d-flex mt-5'>
+        <input type='text' className='form-control' value={description} onChange={e => setDescription(e.target.value)}/>
+        <button className='btn btn-success'>Add</button>
+      </form>
     </>
   )
 }
