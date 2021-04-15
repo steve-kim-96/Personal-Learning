9283bd3c1c3792227d6fb94704d78f09d9912913
steve-kim-96 <soungku@hotmail.com>
2021-04-15 16:14:30 +1200

    create and update tree data structure

--- random-functions/Trees/trees.js
+++ random-functions/Trees/trees.js
@@ -1,6 +1,17 @@
 class TreeNode {
   constructor(data) {
+    // the tree node takes in some sort of information as the data and also has reference to its children nodes
     this.data = data;
     this.children = [];
+  }
+
+  //adding a child node
+  //allow the user to either enter an instance of TreeNode or just enter the child and have the functinon instantiate the new TreeNode and add as child
+  addChild(child) {
+    if (child instanceof TreeNode) {
+      this.children.push(child)
+      return
+    }
+    this.children.push(new TreeNode(child))
   }
 }
