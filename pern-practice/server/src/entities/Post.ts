7afde2e1525fd0abecbb1b85a51ef070b8725eab
steve-kim-96 <soungku@hotmail.com>
2021-05-29 17:31:18 +1200

    create entity in ts

---
+++ pern-practice/server/src/entities/Post.ts
@@ -0,0 +1,25 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  text: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
