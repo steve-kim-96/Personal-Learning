715c348f6afd215c8d5a4e678ef54940dbeaa967
steve-kim-96 <soungku@hotmail.com>
2021-05-29 17:44:20 +1200

    extend baseentity to post entity

--- pern-practice/server/dist/entities/Post.js
+++ pern-practice/server/dist/entities/Post.js
@@ -11,7 +11,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Post = void 0;
 const typeorm_1 = require("typeorm");
-let Post = class Post {
+let Post = class Post extends typeorm_1.BaseEntity {
 };
 __decorate([
     typeorm_1.PrimaryGeneratedColumn(),
