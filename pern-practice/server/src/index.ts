f7c2f6e170336b4ddc6510ad0411a53fe4b59552
steve-kim-96 <soungku@hotmail.com>
2021-05-29 17:35:07 +1200

    attempt connection to postgres database

--- pern-practice/server/src/index.ts
+++ pern-practice/server/src/index.ts
@@ -1,9 +1,33 @@
+import "reflect-metadata";
 import express from "express";
 import "dotenv-safe/config";
+import { createConnection } from "typeorm";
+import path from "path";
+import { Post } from "./entities/Post";
 
-const app = express();
-const port = process.env.PORT;
+const port = 4000;
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+const main = async () => {
+  const connection = await createConnection({
+    type: "postgres",
+    database: "pern",
+    username: "postgres",
+    password: "Passw00rd12!",
+    logging: true,
+    synchronize: true,
+    migrations: [path.join(__dirname, "./migrations/*")],
+    entities: [Post],
+  });
+
+
+
+  const app = express();
+
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+};
+
+main().catch((error) => {
+  console.error(error);
 });
